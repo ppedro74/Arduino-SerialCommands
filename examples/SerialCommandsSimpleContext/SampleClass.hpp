6288fbec4784761e31189f33177d379ff6461750
Aurelien Labrosse <aurelienl@sogilis.com>
2023-11-06 10:34:07 +0100

    feat: Add sample sketch for context usage

---
+++ examples/SerialCommandsSimpleContext/SampleClass.hpp
@@ -0,0 +1,13 @@
+
+/*
+ * Class declaration does not mention SerialCommands, 
+ * hidden in implementation
+ */
+class SampleClass {
+
+public:
+  SampleClass(char ledPin);
+  void loop();
+  char ledOnCounter;
+  char ledPin;
+};
